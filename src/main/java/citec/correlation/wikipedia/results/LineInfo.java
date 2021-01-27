bbf13e1f5b2e552693771c192b90e6cdcccd1415
Mohammad Fazleh Elahi <fazleh2010@gmail.com>
2021-01-27 11:21:15 +0100

    thresold check

--- src/main/java/citec/correlation/wikipedia/results/LineInfo.java
+++ src/main/java/citec/correlation/wikipedia/results/LineInfo.java
@@ -35,7 +35,14 @@ public class LineInfo {
     private Integer nGramNumber = 0;
     private Map<String, Double> probabilityValue = new TreeMap<String, Double>();
     private Analyzer analyzer = null;
+    public static String CHECK_THRESOLD_VALUE="CHECK_THRESOLD_VALUE";
+    
+    //supA=93, supB=115, supAB=93, condBA=1, condAB=0.808, AllConf=0.808, Coherence=0.447, Cosine=0.899, Kulczynski=0.904, MaxConf=1, IR=0.191
 
+    
+    public LineInfo(){
+        
+    }
     public LineInfo(String className, String line, Integer wordIndex, Integer kbIndex) throws Exception {
         this.line = line;
         this.className = className;
@@ -55,7 +62,25 @@ public class LineInfo {
             this.setRule();
             this.setProbabilityValue(line);
         }
+        
     }
+    
+    public static Boolean isThresoldValid(Map<String, Double> probabilityValue, Map<String, Double> thresolds) throws Exception {
+        boolean thresoldValid = true;
+        for (String attribute : probabilityValue.keySet()) {
+            if (probabilityValue.containsKey(attribute) && thresolds.containsKey(attribute)) {
+                Double probValue = probabilityValue.get(attribute);
+                Double thresold = thresolds.get(attribute);
+                if (probValue >thresold) {
+                    thresoldValid = true;
+                }
+                else
+                    return false;
+            }
+        }
+        return thresoldValid;
+    }
+
 
    
     
@@ -83,12 +108,26 @@ public class LineInfo {
         this.rule = str;
     }
 
-    private void setProbabilityValue(String line) {
+    /*private void setProbabilityValue(String line) {
         line = line.replace("AllConf", "[AllConf");
         line = line + "]";
         String values = StringUtils.substringBetween(line, "[AllConf", "]").replace(",", "");
         String[] info = values.split(" ");
         for (Integer i = 0; i < info.length; i++) {
+            Pair<String, String> pair = this.setValue(info[i]);
+            double dnum = Double.parseDouble(pair.getValue1());
+            this.probabilityValue.put(pair.getValue0(), dnum);
+        }
+    }*/
+    
+    private void setProbabilityValue(String line) {
+        line = line.replace("supA=", "$supA=");
+        line = line + "$";
+        String values = StringUtils.substringBetween(line, "$", "$").replace(",", "");
+         System.out.println(values);
+        String[] info = values.split(" ");
+        for (Integer i = 0; i < info.length; i++) {
+            System.out.println(info[i]);
             Pair<String, String> pair = this.setValue(info[i]);
             double dnum = Double.parseDouble(pair.getValue1());
             this.probabilityValue.put(pair.getValue0(), dnum);
@@ -242,6 +281,34 @@ public class LineInfo {
         return "LineInfo{" + "line=" + line + ", subject=" + subject + ", predicate=" + predicate + ", object=" + object + ", rule=" + rule + ", word=" + word + ", probabilityValue=" + probabilityValue + '}';
     }
 
-   
+    public static void main(String[] args) throws Exception {
+        String line = "dbo:AdministrativeRegion in c_e and exists s : (s, dbp:admCtrOf, e) in G } => { occurs('Russian', d_e) } | supA=60, supB=60, supAB=60, condBA=1, condAB=1, AllConf=1, Coherence=0.5, Cosine=1, Kulczynski=1, MaxConf=1, IR=0";
+        LineInfo lineInfo = new LineInfo();
+        lineInfo.setProbabilityValue(line);
+        System.out.println(lineInfo.getProbabilityValue());
+        Map<String, Double> probabilityValue = new TreeMap<String, Double>();
+        probabilityValue.put("AllConf", 1.0);
+        probabilityValue.put("Coherence", 0.5);
+        probabilityValue.put("Cosine", 1.0);
+        probabilityValue.put("IR", 1.0);
+        probabilityValue.put("Kulczynski", 1.0);
+        probabilityValue.put("MaxConf", 1.0);
+        probabilityValue.put("condAB", 1.0);
+        probabilityValue.put("condBA", 1.0);
+        probabilityValue.put("supA", 1.0);
+        probabilityValue.put("supAB", 60.0);
+        probabilityValue.put("supB", 60.0);
+        Map<String, Double> thresolds = new TreeMap<String, Double>();
+        thresolds.put("supA", 0.0);
+        thresolds.put("supAB", 50.0);
+        thresolds.put("Cosine", 0.0);
+        if (LineInfo.isThresoldValid(probabilityValue, thresolds)) {
+            System.out.println("valid line:" + line);
+        }
+        else
+             System.out.println("invalid line:" );
+                
+    }
+
 
 }
